b1df3e3927db6a76092ad3e097147600c63949e3
Martin Burolla <mburolla@xpanxion.com>
2022-07-30 17:08:56 -0400

    update

--- Assignments/stu0/iga/iga.py
+++ Assignments/stu0/iga/iga.py
@@ -5,34 +5,10 @@
 # Desc: Car Cost
 #
 
-def calc_total_cost(car_list):
-    total_cost = 0
-    for c in car_list:
-        if c["color"] == "red":
-            total_cost += int(c["data"][6:])
-    return f"Total cost: {total_cost}."
 
 
 def iga():
-    car_list = [
-        {
-            "car_id": 1,
-            "color": "red",
-            "data": "Cost: 20000"
-        },
-        {
-            "car_id": 2,
-            "color": "red",
-            "data": "Cost: 30000"
-        },
-        {
-            "car_id": 3,
-            "color": "yellow",
-            "data": "Cost: 30000"
-        }
-    ]
-
-    print(calc_total_cost(car_list))
+   print("IGA...")
 
 
 if __name__ == '__main__':
